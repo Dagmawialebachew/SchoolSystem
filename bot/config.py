c20a22da0ad905f322fb5f0b66a84def87a13184
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-13 14:45:32 +0300

    Add config.py

---
+++ bot/config.py
@@ -0,0 +1,3 @@
+# bots/config.py
+TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k"  # your actual bot token
+DJANGO_API_URL = "https://schoolsys.pythonanywhere.com/parents/api/save_chat_id/"
