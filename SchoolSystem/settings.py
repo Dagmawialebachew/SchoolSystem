d649391286ea5b7d38b11e34539a9e2b76b5e05a
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-13 15:39:07 +0300

    Fix database issues

--- SchoolSystem/settings.py
+++ SchoolSystem/settings.py
@@ -99,14 +99,11 @@ WSGI_APPLICATION = 'SchoolSystem.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.getenv("DB_NAME", BASE_DIR / "db.sqlite3"),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
-        "PORT": os.getenv("DB_PORT", ""),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # full path to your SQLite file
     }
 }
+
 
 # Email
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
