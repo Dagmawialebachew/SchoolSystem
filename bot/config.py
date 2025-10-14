62c314ab99aceef4892d9abb6f12d42dce9f776a
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-14 19:01:34 +0300

    Update main.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,7 @@
 # bots/config.py
-TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k"  # your actual bot token
-DJANGO_API_URL_CONNECT = "https://schoolsys.pythonanywhere.com/parents/api/save_chat_id/"
-# DJANGO_API_URL_DISCONNECT = "https://schoolsys.pythonanywhere.com/parents/api/disconnect_chat_id/"
-DJANGO_API_URL_DISCONNECT = "https://schoolsys.pythonanywhere.com/parents/api/disconnect_chat_id/"
+
+TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k" 
+
+# CHANGE 'https' TO 'http' for internal PythonAnywhere calls
+DJANGO_API_URL_CONNECT = "http://schoolsys.pythonanywhere.com/parents/api/save_chat_id/"
+DJANGO_API_URL_DISCONNECT = "http://schoolsys.pythonanywhere.com/parents/api/disconnect_chat_id/"
