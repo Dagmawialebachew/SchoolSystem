ad8178a74d3ad23a35126a3b5b43093ba049c513
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-16 22:28:09 +0300

    Update the bot

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@
 TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k" 
 
 # CHANGE 'https' TO 'http' for internal PythonAnywhere calls
-DJANGO_API_URL_CONNECT = "https://schoolsys.pythonanywhere.com/parents/api/save_chat_id/"
-DJANGO_API_URL_DISCONNECT = "https://schoolsys.pythonanywhere.com/parents/api/disconnect_chat_id/"
-DJANGO_API_URL_FEE = "https://schoolsys.pythonanywhere.com/parents/api/parent/"
-WEB_APP_BASE_URL = "https://schoolsys.pythonanywhere.com" # Must be set to 'http://schoolsys.pythonanywhere.com'
+DJANGO_API_URL_CONNECT = "http://schoolsys.pythonanywhere.com/parents/api/save_chat_id/"
+DJANGO_API_URL_DISCONNECT = "http://schoolsys.pythonanywhere.com/parents/api/disconnect_chat_id/"
+DJANGO_API_URL_FEE = "http://schoolsys.pythonanywhere.com/parents/api/parent/"
+WEB_APP_BASE_URL = "http://schoolsys.pythonanywhere.com" # Must be set to 'http://schoolsys.pythonanywhere.com'
