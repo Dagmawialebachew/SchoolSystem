e7e4237eeacb1d42f697d219b3b4840484984dc0
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-16 23:24:31 +0300

    Update the bot

--- bot/config.py
+++ bot/config.py
@@ -3,11 +3,11 @@ TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k"
 
 # --- INTERNAL API URLs (Bot ↔ Django Communication) ---
 # Use HTTPS so PythonAnywhere reliably routes requests to your Django app
-BASE_API_URL_INTERNAL = "https://schoolsys.pythonanywhere.com"
+BASE_API_URL_INTERNAL = "http://schoolsys.pythonanywhere.com"
 DJANGO_API_URL_CONNECT = f"{BASE_API_URL_INTERNAL}/parents/api/save_chat_id/"
 DJANGO_API_URL_DISCONNECT = f"{BASE_API_URL_INTERNAL}/parents/api/disconnect_chat_id/"
 DJANGO_API_URL_FEE = f"{BASE_API_URL_INTERNAL}/parents/api/parent/"
 
 # --- EXTERNAL WEB APP URL (For Telegram buttons/links) ---
 # Must be publicly accessible over HTTPS for Telegram Web App buttons
-WEB_APP_BASE_URL = "https://schoolsys.pythonanywhere.com"
+WEB_APP_BASE_URL = "http://schoolsys.pythonanywhere.com"
