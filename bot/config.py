73c875881ce71f52c0a6b67113a15a5c0e8dd7c3
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-10-17 12:31:54 +0300

    Update the main.py, ensuring connections are secure

--- bot/config.py
+++ bot/config.py
@@ -5,9 +5,7 @@ TELEGRAM_BOT_TOKEN = "8141768447:AAE-sk9IROgjZWmaJEI5iU4R9rL1QyzrV7k"
 
 # --- INTERNAL API URLs (Bot ↔ Django Communication) ---
 # Use the **public HTTPS** domain for cross-process access.
-BASE_API_URL_INTERNAL = "https://schoolsys.pythonanywhere.com"
-
-# Define API Endpoints
+BASE_API_URL_INTERNAL = "http://127.0.0.1:8000"
 DJANGO_API_URL_CONNECT = f"{BASE_API_URL_INTERNAL}/parents/api/save_chat_id/"
 DJANGO_API_URL_DISCONNECT = f"{BASE_API_URL_INTERNAL}/parents/api/disconnect_chat_id/"
 DJANGO_API_URL_FEE = f"{BASE_API_URL_INTERNAL}/parents/api/parent/"
